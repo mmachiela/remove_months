ebbbcce2db0f446e7faa19256819e1375953e484
Mathijs Machiela <43295032+mmachiela@users.noreply.github.com>
2018-10-26 16:30:59 +0200

    Add remove_months.ps1

---
+++ remove_months.ps1
@@ -0,0 +1,51 @@
+param(
+[string]$Path
+)
+
+$runDate = Get-Date
+
+$Date = (Get-Date).AddMonths(-2)
+
+$monthDelete = Get-Date $Date -Format "yyyyMM"
+
+#$monthDelete = "201806"
+
+$arrFilter = @("MA13-ILMC","MA14-ILMS","MA17-ILMV","MA51-VDO1","MA80-ILM2D")
+
+$arrRootFolders = get-childitem $Path -Exclude $arrFilter -Force -OutBuffer 1000 | where {$_.Attributes -match 'Directory'}
+
+Write-Host "Cleanup Script Running - " $runDate
+Write-Host "Path to cleanup: " $Path
+Write-Host "Month to Cleanup: " $monthDelete
+Write-Host "-------------"
+Write-Host "Exclusion filters: "
+
+Foreach ($filter in $arrFilter)
+{
+    Write-Host $filter   
+}
+
+Write-Host "-------------"
+Write-Host "Start Cleanup!"
+Write-Host "-------------"
+
+
+foreach ($item in $arrRootfolders)
+{
+    Write-Host "Check Folder: " $item.FullName
+
+    $arrSubfolders = get-childitem $Item.FullName -Recurse -Force -OutBuffer 1000 -Directory $monthDelete | where {$_.Attributes -match 'Directory'}
+        
+    foreach ($subFolder in $arrSubfolders)
+    {
+        Write-Host "!!! DELETE FOLDER: " $subFolder.FullName
+        remove-item $subfolder.FullName -Recurse -Force
+    }
+            
+}
+
+$endDate = Get-Date
+
+Write-Host "-------------"
+Write-Host "Script ended! - " $endDate
+Write-Host "-------------"
